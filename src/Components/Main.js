4e12e514784ccec3ff109f957b6cdee2c57c50e4
Medasele273 <medasele273@gmail.com>
2020-02-25 08:10:24 +0100

    file edited

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -10,7 +10,7 @@ const Main = () => (
 
 
     <Switch>
-        <Route exact path="/">
+        <Route exact path="/home">
             <LandingPage />
         </Route>
         <Route path="/home">
