d99e397be8e4ca940f02b6cd5dba091b2d0f9720
Medasele273 <medasele273@gmail.com>
2020-02-25 07:59:03 +0100

    Updates

---
+++ precache-manifest.d34bae2c40e9fcf902f4a165e66cbdc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f01cd78ceeed32da20b536bf5f02488",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "b4b89ecdd15a16e1f997",
+    "url": "/portfolio2/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "689af66c8bce9157eca2",
+    "url": "/portfolio2/static/css/main.fcaae77f.chunk.css"
+  },
+  {
+    "revision": "b4b89ecdd15a16e1f997",
+    "url": "/portfolio2/static/js/2.78cf6669.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/portfolio2/static/js/2.78cf6669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "689af66c8bce9157eca2",
+    "url": "/portfolio2/static/js/main.5f59f818.chunk.js"
+  },
+  {
+    "revision": "7c590332b093942314b2",
+    "url": "/portfolio2/static/js/runtime-main.6744596b.js"
+  }
+]);
